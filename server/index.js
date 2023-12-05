dea75eac7728c89eefd6e4470301516bc7b82099
Raja Kumar <rajakumarmahto952@gmail.com>
2023-12-05 16:06:59 +0530

    api updated

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
-const mongoURI = process.env.MONGO_URI;
+const mongoURL = process.env.MONGO_URL;
 const port = process.env.PORT || 4000;
 
-const url = `${mongoURI}`;
-const dbName = "wazirx-all";
+const url = `${mongoURL}`;
+const dbName = "wazirx";
 const collectionName = "tickers";
 app.use(cors());
 const axios = require("axios");
@@ -57,25 +57,26 @@ cron.schedule("*/2 * * * *", async () => {
     }
 
     // Connect to MongoDB Atlas
-    const client = new MongoClient(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    await client.connect();
+    const client = new MongoClient(url);
 
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection(collectionName);
 
-    // Clear existing data
-    await collection.deleteMany({});
+      // Clear existing data
+      await collection.deleteMany({});
 
-    // Insert the new data
-    await collection.insertMany(first10Items);
+      // Insert the new data
+      await collection.insertMany(first10Items);
 
-    // Close the MongoDB connection
-    await client.close();
-
-    console.log("Task completed successfully");
+      console.log("Task completed successfully");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+    } finally {
+      // Close the MongoDB connection
+      await client.close();
+    }
   } catch (error) {
     console.error("Error fetching data:", error);
   }
@@ -84,23 +85,28 @@ cron.schedule("*/2 * * * *", async () => {
 app.get("/getTickers", async (req, res) => {
   try {
     // Connect to MongoDB Atlas
-    const client = new MongoClient(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    await client.connect();
+    const client = new MongoClient(url);
 
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection(collectionName);
 
-    // Find all documents in the collection and convert them to an array
-    const tickerData = await collection.find({}).toArray();
+      // Find all documents in the collection and convert them to an array
+      const tickerData = await collection.find({}).toArray();
 
-    // Close the MongoDB connection
-    await client.close();
+      // Close the MongoDB connection
+      await client.close();
 
-    // Send the ticker data as JSON response
-    res.json(tickerData);
+      // Send the ticker data as JSON response
+      res.json(tickerData);
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      res.status(500).json({ error: "An error occurred" });
+    } finally {
+      // Close the MongoDB connection
+      await client.close();
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred" });
@@ -109,20 +115,23 @@ app.get("/getTickers", async (req, res) => {
 
 app.get("/getDocuments", async (req, res) => {
   try {
-    const client = new MongoClient(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    await client.connect();
+    const client = new MongoClient(url);
 
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection(collectionName);
 
-    const documents = await collection.find({}).limit(10).toArray();
+      const documents = await collection.find({}).limit(10).toArray();
 
-    client.close();
-
-    res.json(documents);
+      res.json(documents);
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    } finally {
+      // Close the MongoDB connection
+      await client.close();
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
